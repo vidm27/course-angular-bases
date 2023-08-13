61751b5c7e125c01d329e1b828e603d008dd1720
vidm27 <davidmoran464@gmail.com>
2023-08-13 17:00:00 -0500

    Eventos en componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Mi primera app de Angular, no se ve dificil'; // son propiedad de Angular
   public counter: number = 10;
-
-  increaseBy(value: number): void{
-    this.counter +=1;
-  }
-
-  descreassBy(value: number): void{
-    this.counter -=1;
+  increaseBy(value: number): void {
+    this.counter += value;
   }
 
   resetCount(): void {
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,9 +3,22 @@ import { Component } from "@angular/core";
 // es un decorador que transforma mi clase
 @Component({
   selector: 'app-counter', // es recomendable utilizar prefijos para los nombres de los componentes
-  template: '<h1>Hola counter</h1>'
+  template: `<h1>Hola counter</h1>
+  <p>Counter: {{counter}}</p>
+<button (click)="increaseBy(1)">+1</button>
+<button (click)="resetCount()">Reset</button>
+<button (click)="increaseBy(-1)">-1</button>
+  `
 })
 
-export class CounterComponent{
+export class CounterComponent {
+  public counter: number = 0;
 
+  increaseBy(value: number): void{
+    this.counter += value;
+  }
+
+  resetCount(): void {
+    this.counter = 10;
+  }
 }
