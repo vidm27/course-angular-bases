ae3e39df6818a675449d54e15f8c02ac20cc679b
vidm27 <davidmoran464@gmail.com>
2023-08-15 19:35:45 -0500

    feat: using ngfor, ngtemplate y ngif

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Black Panther']
+  public deleteHero?: string;
+
+
+  removeLastHero(): void {
+    this.deleteHero = this.heroNames.pop();
+    // console.log({ deleteHero });
+  }
 }
