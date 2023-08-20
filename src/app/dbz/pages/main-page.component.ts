f651b89f8537856ad3c4c6d2263af748daac2133
vidm27 <davidmoran464@gmail.com>
2023-08-20 11:32:19 -0500

    feat: Agregando comunicacion entre el padre y el hijo

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-main-page',
+  selector: 'dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 
-export class MainPageComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit() { }
+export class MainPageComponent {
+  public characters: Character[] = [{
+    name: 'Krilin',
+    power: 1000
+  }, {
+    name: 'Goku',
+    power: 9500
+  },
+  { name: 'Vegeta', power: 10000 }]
 }
